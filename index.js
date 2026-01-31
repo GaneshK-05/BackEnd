1f0c08bee35ea2888213e9f8f85650013caa97b0
Ganesh K <kkganesh2005@gmail.com>
2026-01-31 14:25:45 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv'
+import cors from 'cors'
+import cookieParser from "cookie-parser";
+import connectDB from './Database/connection.js';
+import authRouter from './Routers/authRouter.js';
+import adminRouter from './Routers/adminRouter.js';
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.get("/", (req,res) => {
+    res.status(200).json({ msg: "server is running in",port});
+});
+
+app.use("/api/v1/auth", authRouter);
+
+app.use("/api/v1/admin", adminRouter );
+
+app.listen(port, () =>{
+    connectDB();
+    console.log("your server is running in port",port);
+})
